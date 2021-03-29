370dd80e3c1ba37a3eb99a3845fdc7ecb0eacb1c
Adin W-T <adinrwt@gmail.com>
2021-03-29 22:55:38 +0000

    The javascript now disables the compress button if the server is compressing at page load time or if it is pressed once
    The onLoad() javascript function is run when the file is loaded in the
    header.  It checks if the server is compressing and both reports the
    times the movie has been compressed and disables the compression button
    if necessary.
    
    The compress button styling has also been commented out because it made
    it impossible to tell if it was disabled.

--- static/scripts/compress.js
+++ static/scripts/compress.js
@@ -1,5 +1,25 @@
+// TODO(Adin): Remove all logging in production
+
+function compressionDoneCallback(data) {
+    console.log("Compression success: " + data.success);
+
+    if(data.success) {
+        $("#compressButton").prop("disabled", true);
+    }
+}
+
 function compress() {
-    $.post("/compress", function(data) {
-        console.log(data.success);
+    $.post("/compress", compressionDoneCallback, "json");
+}
+
+function onLoad() {
+    $.get("/compress", function(data) {
+        $("#times_compressed").html("Compressed " + data.num_compressions + " Times");
+
+        if(data.is_compressing) {
+            $("#compressButton").prop("disabled", true);
+        }
     }, "json");
 }
+
+onLoad();
