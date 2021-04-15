25370e4077b57a7e50371df1b0e540c3528b1f7e
Adin W-T <adinrwt@gmail.com>
2021-04-15 06:35:32 -0400

    Added refresh and eventsource handling to the javascript
    The page refreshes when the state changes from copmpressing to not
    compressing and eventsource is created and closed based on when the page
    becomes visible or hidden.

--- static/scripts/compress.js
+++ static/scripts/compress.js
@@ -1,6 +1,14 @@
 // TODO(Adin): Remove all logging in production
+// TODO(Adin): Remove keep_alive event callback
 
-function updatePage() {
+// Variable to store previous state of compression
+// before updatePage has finished so that when the
+// state goes from compressing to not compressing 
+// the page can be refreshed
+var wasCompressing = false;
+var eventSource;
+
+function updatePage(fromMessage) {
     $.get("/compress", function(data) {
         console.log("updatePage: " + data.is_compressing + ", " + data.num_compressions)
         if(data.is_compressing) {
@@ -11,28 +19,45 @@ function updatePage() {
         }
 
         $("#times_compressed").html("Compressed " + data.num_compressions + " Times");
+
+        if(fromMessage && wasCompressing) {
+            // Refresh if compression is done
+            location.reload();
+        }
+
+        wasCompressing = data.is_compressing;
     }, "json");
 }
 
 function compress() {
     $.post("/compress", null, "json");
-    updatePage();
+    updatePage(false);
 }
 
-eventSource = new EventSource("/events");
-eventSource.addEventListener("keep_alive", function(event) {
-    console.log(event);
-});
+function onVisibilityChange() {
+    if(document.visibilityState == "visible") {
+        eventSource = new EventSource("/events");
+        eventSource.addEventListener("keep_alive", function(event) {
+            console.log(event);
+        });
 
-eventSource.addEventListener("compress", function(event) {
-    console.log(event);
-    updatePage();
-});
+        eventSource.addEventListener("compress", function(event) {
+            console.log(event);
+            updatePage(true);
+        });
 
-eventSource.onerror = function() {
-    console.log("EventSource failed.");
-};
+        eventSource.onerror = function() {
+            console.log("EventSource failed.");
+        };
+    }
+    else if(eventSource != null) {
+        eventSource.close();
+    }
+
+}
 
 window.onload = function() {
-    updatePage();
+    document.addEventListener("visibilitychange", onVisibilityChange);
+
+    updatePage(false);
 }
